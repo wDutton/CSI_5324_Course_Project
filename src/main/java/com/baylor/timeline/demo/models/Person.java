693b81d7619890180b5c12d0e1f4f1fb47725c80
Jake Formico <jakeformico@gmail.com>
2023-03-28 10:02:58 -0500

    Added connection to postgres database

--- src/main/java/com/baylor/timeline/demo/models/Person.java
+++ src/main/java/com/baylor/timeline/demo/models/Person.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -21,23 +22,47 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Nonnull
+    @Column(nullable = false)
     private String firstName;
 
-    @Nonnull
+    @Column(nullable = false)
     private String lastName;
 
-    @Nonnull
+    @Column(nullable = false)
     private String email;
 
     @OneToMany(cascade = CascadeType.MERGE)
     private List<Post> postList = new ArrayList<Post>();
 
-    public void addContent(Post post){
+    public void addContent(Post post) {
         this.postList.add(post);
     }
- 
+
     public enum Type {
         OfficeManager, Student, Faculty, Admin
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/test/java/com/baylor/timeline/demo/DemoApplicationTests.java
+++ src/test/java/com/baylor/timeline/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.baylor.timeline.demo;
+// package com.baylor.timeline.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+// @SpringBootTest
+// class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// @Test
+// void contextLoads() {
+// }
 
-}
+// }
